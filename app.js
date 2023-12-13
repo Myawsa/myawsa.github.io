87229a3dad692e7fbac42774530861cb16bce8f6
Myawsa <152040714+Myawsa@users.noreply.github.com>
2023-12-13 10:46:41 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyB4UAc4CeB-araFa8qlTlRcEQl1Z-h04HM",
+    authDomain: "tomzzproject-fadb4.firebaseapp.com",
+    databaseURL: "https://tomzzproject-fadb4-default-rtdb.firebaseio.com",
+    projectId: "tomzzproject-fadb4",
+    storageBucket: "tomzzproject-fadb4.appspot.com",
+    messagingSenderId: "682941877127",
+    appId: "1:682941877127:web:3cf3ad2d2ecebbdce3a122"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
